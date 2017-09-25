f1628ee381ff6ab29e11a2646d5947039ca0bd1f
CrossroadsMan <crossroadsman@gmail.com>
2017-09-25 15:07:22 -0600

    Fix typo

--- simple_app.py
+++ simple_app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 # `debug=True`: automatically restart the server if there is an error
 # `port=8000`: listen on port 8000 (a common port for web servers)
 # `host='0.0.0.0': listen on all addresses that can reach this app
-app.run(debug=True, port=8000, host-'0.0.0.0')
+app.run(debug=True, port=8000, host='0.0.0.0')
 
