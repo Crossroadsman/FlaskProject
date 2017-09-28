59b02eb4bae358a7008e119e7a5da277a10e8f2a
CrossroadsMan <crossroadsman@gmail.com>
2017-09-28 14:21:50 -0600

    Implement form ability to call save() function in code

--- forms_demo/app.py
+++ forms_demo/app.py
@@ -6,6 +6,20 @@ app = flask.Flask(__name__)
 def index():
     return flask.render_template('index.html')
 
+# Here we are creating a route and a method to handle a user submitting the call to action button on the index page
+#
+# We have dedicated a uri (`/save`) to identify this action
+# Note that we are also restricting access to this view to only http POST methods
+@app.route('/save', methods=['POST'])
+def save():
 
+    name = flask.request.args.get('name', None)
+
+    if name == None:
+        print("You forgot to add a name")
+    else:
+        print("{} is a nice name. I like it.".format(name))
+
+    return "process completed"
 
 app.run(debug=True, host='0.0.0.0', port=8000)
