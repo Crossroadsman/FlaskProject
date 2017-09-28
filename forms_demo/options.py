b3c67a70a85f6c733256e11279aaea787c055dee
CrossroadsMan <crossroadsman@gmail.com>
2017-09-28 13:33:33 -0600

    Start implementing demo with forms

---
+++ forms_demo/options.py
@@ -0,0 +1,49 @@
+DEFAULTS = {
+    "shirts" : [
+        'green',
+        'blue',
+        'black',
+        'white',
+        'yellow',
+        'red',
+        'orange'],
+    "trousers" : [
+        'blue jeans',
+        'black jeans',
+        'black sweat pants',
+        'grey sweat pants',
+        'black slacks',
+        'blue slacks',
+        'grey slacks'],
+    "hats" : [
+        'red baseball cap',
+        'blue baseball cap',
+        'black baseball cap',
+        'beige tilley hat',
+        'grey tilley hat',
+        'olive tilley hat',
+        'black toque',
+        'blue toque',
+        'grey toque'],
+    "glasses" : [
+        'sunglasses',
+        'regular glasses'],
+    "footwear" : [
+        'white running shoes',
+        'black running shoes',
+        'grey running shoes',
+        'black loafers',
+        'tan loafers'],
+    "colours" : [
+        'black',
+        'grey',
+        'white',
+        'blue',
+        'red',
+        'green',
+        'yellow',
+        'orange',
+        'purple',
+        'pink']
+}
+    
